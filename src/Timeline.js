4cc0d00cc7ee589cf54b59274c4888b4380da8fc
Harpo <harpo@harpo.me>
2019-12-22 16:55:18 -0800

    working in/out point updating!!!

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,7 +1,6 @@
 import m from 'mithril'
 import interact from 'interactjs'
 // import dragula from 'dragula'
-// var resizable = require("./resizable")
 import Sortable from 'sortablejs'
 var state = require("./Globals").state
 
@@ -9,119 +8,44 @@ var state = require("./Globals").state
 var clips = 0
 var p = {x: 0, y: 0}
 var scale = 10
-// var dragOpts = {
-  // invalid: (el, handle) => {
-  //   console.log(el, handle, handle.classList.contains('handle'))
-  //   if (handle.classList.contains('handle')) {
-  //     return true
-  //   } else {
-  //     return false
-  //   }
-  // }
-
-  // cursorChecker: (action, interactable, element, interacting) => {
-  //   console.log(action, interactable, element, interacting, state.resizing())
-  //   if (action.edges) {
-  //   state.resizing(true)
-  // } else {
-  //   state.resizing(false)
-  // }
-    // return false
-  // }
-// }
-//
-// function startResizing(e) {
-//   let el = e.toElement.offsetParent
-//   console.log('resizing', e, el)
-//    el.style.width = (e.clientX - el.offsetLeft) + 'px';
-//    // el.style.width += e.movementX
-//    // this.style.height = (e.clientY - this.offsetTop) + 'px';
-// }
-// function stopResizing(e) {
-//   let el = e.toElement.offsetParent
-//     document.removeEventListener('mousemove', startResizing, false);
-//     document.removeEventListener('mouseup', stopResizing, false);
-// }
 
 class Clip {
-  // constructor(vnode) {
-    // this.dom = vnode.dom
-    // console.log('clip init', vnode)
-
-
-    // this.initialiseResize()
-  // }
-
-//
-// initialiseResize(e) {
-//   let el = e.toElement
-//   let p = el.offsetParent
-//   console.log('initialized resizer', e, el, p)
-//   document.addEventListener('mousemove', startResizing, false);
-//   document.addEventListener('mouseup', stopResizing, false);
-// }
+  constructor(vnode) {
+    this.inpoint = vnode.attrs.inpoint
+    this.outpoint = vnode.attrs.outpoint
+    this.description = vnode.attrs.description
+    console.log('clip init', vnode, this.inpoint, this.outpoint)
+    }
 
   oninit(vnode) {
   }
   oncreate(vnode) {
-    this.dom = vnode.dom
-    // console.log('created!', this.dom)
-    // this.initialiseResize(vnode)
-    // vnode.state.inpoint =
-  }
-  view(vnode) {
-    return m(`.clip#${vnode.attrs.name}`, {'data-x': vnode.attrs.inpoint/scale, 'data-duration': vnode.attrs.duration/scale, innerHTML: vnode.attrs.description}, [
-      // m('.handle lh', {onmousedown: this.initialiseResize}),
-      // m('.handle rh', {onmousedown: this.initialiseResize})
-    ])
-      // {data-x: vnode.attrs.inpoint/scale, data-y: vnode.attrs.outpoint/scale})
-  }
-}
-
-var Timeline = {
-  edl: [],
-  oninit: (vnode) => {
-    const clip = interact('.clip')
-
-    // clip.draggable({
-    //   inertia: true,
-    //   // lockAxis: 'x',
-    //   modifiers: [
-    //     interact.modifiers.restrictRect({
-    //       restriction: 'parent',
-    //       endOnly: true,
-    //     })
-    //   ],
-    //   autoscroll: true,
-    //   listeners: {
-    //     move: (event) => {
-    //       let target = event.target
-    //       var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-    //       var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-    //       // p.x += event.dx
-    //       // p.y += event.dy
-    //       target.style.webkitTransform = target.style.transform = `translate(${x}px,${y}px)`
-    //       target.setAttribute('data-x', x)
-    //       target.setAttribute('data-y', y)
-    //     }
-    //   },
-    // })
-
+    const clip = interact(vnode.dom)
     clip.on('resizemove', (event) => {
       state.resizing(true)
       let target = event.target
+      target.style.width  = event.rect.width + 'px';
       // let { x, y } = event.target.dataset
-      var x = (parseFloat(target.getAttribute('data-x')) || 0)
-      var y = (parseFloat(target.getAttribute('data-y')) || 0)
+      // var x = (parseFloat(target.getAttribute('data-x')) || 0)
+      // var y = (parseFloat(target.getAttribute('data-y')) || 0)
       // console.log('x,y: ', x, y)
 
-      target.style.width  = event.rect.width + 'px';
-      x += event.deltaRect.left;
+      // x += event.deltaRect.left;
       // target.style.webkitTransform = target.style.transform =
       //   `translate(${x}px,${y}px)`;
 
-    target.setAttribute('data-x', x);
-    // target.textContent = `${event.rect.width}×${event.rect.height}\nx: ${x}\ny: ${y}`;
+    // target.setAttribute('data-x', x);
+    var midpoint = target.offsetLeft + target.offsetWidth / 2
+    console.log('event: ', event) //event.delta.x, midpoint)
+    if (event.page.x < midpoint) {
+      vnode.state.inpoint += event.deltaRect.left
+      console.log('changing inpoint', vnode)
+    } else {
+      vnode.state.outpoint += event.deltaRect.right
+      console.log('changing outpoint', event.deltaRect, midpoint)
+    }
+    target.innerHTML= `${vnode.state.description} - [${vnode.state.inpoint}, ${vnode.state.outpoint}]`
+    // m.redraw()
     })
     clip.resizable({
       edges: {
@@ -137,10 +61,26 @@ var Timeline = {
       state.resizing(false)
       console.log('resize stop', state.resizing())
     })
+  }
+  view(vnode) {
+    return m(`.clip#${vnode.attrs.name}`, {'data-x': vnode.state.inpoint/scale, 'data-duration': vnode.attrs.duration/scale, innerHTML: `${vnode.state.description} - [${vnode.state.inpoint}, ${vnode.state.outpoint}]`, inpoint: vnode.state.inpoint, outpoint: vnode.state.outpoint}, [
+      // m('.handle lh', {onmousedown: this.initialiseResize}),
+      // m('.handle rh', {onmousedown: this.initialiseResize})
+    ])
+      // {data-x: vnode.attrs.inpoint/scale, data-y: vnode.attrs.outpoint/scale})
+  }
+  // onupdate(vnode) {
+  //   console.log('updating', vnode.attrs.inpoint, vnode.attrs.outpoint)
+  //   vnode.dom.innerHTML= `${vnode.attrs.description} - [${vnode.attrs.inpoint}, ${vnode.attrs.outpoint}]`
+  // }
+}
+
+var Timeline = {
+  edl: [],
+  oninit: (vnode) => {
 
   },
   oncreate: (vnode) => {
-    // dragula([vnode.dom], dragOpts)
     new Sortable(vnode.dom, {
       swapThreshold: 0.50,
       animation: 150,
