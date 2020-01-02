51498391760ab840ad9b130c2d411d0fbd1c275d
Harpo <harpo@harpo.me>
2020-01-02 10:49:57 -0800

    added beginning/end icons

--- src/Tools.js
+++ src/Tools.js
@@ -5,12 +5,18 @@ import Video from './Video'
 var state = require("./Globals").state
 
 export default class Tools {
-  
+
   view(vnode) {
     return m('#tools.tools.', {}, [
       m('i.material-icons', {
         onclick: Monitor.play,
-      }, Video.paused ? 'pause' : 'play_arrow')
+      }, Video.paused ? 'pause' : 'play_arrow'),
+      m('i.material-icons', {
+        onclick: (vnode) => {Monitor.seek(0)}
+      }, 'first_page'),
+      m('i.material-icons', {
+        onclick: (vnode) => {Monitor.seek(Video.duration)}
+      }, 'last_page'),
     ])
   }
 }
