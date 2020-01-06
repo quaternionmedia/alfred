74f75a127c07499ecfcfb62094a024fade389fda
Harpo <harpo@harpo.me>
2020-01-05 17:20:34 -0800

    working contenteditable clip descriptions!

--- src/Clip.js
+++ src/Clip.js
@@ -5,6 +5,16 @@ var Timeline = require("./Timeline")
 import Monitor from './Monitor'
 import { Video, Edl } from './Video'
 
+function whichAmI(e) {
+  console.log('finding index of ', e)
+  let c = e.parentElement.children
+  for (var i = 0; i < c.length; i++) {
+    if (c[i] == e.target) {
+      return i
+    }
+  }
+}
+
 export default class Clip {
   constructor(vnode) {
     this.inpoint = vnode.attrs.inpoint
@@ -52,19 +62,14 @@ export default class Clip {
     //   state.resizing(false)
     //   console.log('resize stop', state.resizing())
     // })
+
     vnode.dom.addEventListener('mousedown', (e) => {
-      e.stopPropagation()
+      // e.stopPropagation()
       const r = e.offsetX / e.target.offsetWidth
       const t = r*(this.outpoint - this.inpoint) + this.inpoint
       console.log('clicked on clip', e, r, t)
       Monitor.seek(t)
-      let c = e.target.parentElement.children
-      for (var i = 0; i < c.length; i++) {
-        if (c[i] == e.target) {
-          Edl.current = i
-          break
-        }
-      }
+      Edl.current = whichAmI(e.target)
       m.redraw()
     }, true)
 
@@ -82,10 +87,11 @@ export default class Clip {
         width: this.outpoint - this.inpoint,
       },
     }, [
-      m('span#clipname.clipname', this.filename.split('/').pop()),
-      m('span#inpoint', this.inpoint),
-      m('span#outpoint', this.outpoint),
-      m('span#description', this.description),
+      m('p#clipname.clipname', this.filename.split('/').pop()),
+      m('p#inpoint[]', this.inpoint),
+      m('p#outpoint[]', this.outpoint),
+      m('p#description[contenteditable=true]', {
+        }, m.trust(this.description)),
       m('i.material-icons#progress.progress',  {
         style: {
           display: (Video.time > this.inpoint) && (Video.time < this.outpoint) ? 'inherit': 'none',
