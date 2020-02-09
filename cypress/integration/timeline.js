d4029df961bdbf6c6ebd13cf34790b0a739e7243
Harpo <harpo@harpo.me>
2020-02-09 09:35:29 -0800

    additional tests for timeline!

--- cypress/integration/timeline.js
+++ cypress/integration/timeline.js
@@ -4,6 +4,13 @@ describe('timeline', () => {
     cy.contains('menu').click()
     cy.contains('projects').click()
     cy.contains('test.csv').click()
+
     cy.url().should('include', '/#!/timeline?edl=test.csv')
+
+    cy.get('#project > tr:nth-child(2)').contains('BigBuckBunny')
+
+    cy.get('#monitor').should('have.attr', 'src').and('eq', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')
+
+    cy.get('#timeline > .clip:nth-child(1)').should('have.attr', 'description').and('eq', 'clip 1')
   })
 })
