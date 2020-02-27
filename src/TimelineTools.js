34343bb3ccb34947e53d72676d887c285dba17c7
Harpo <harpo@harpo.me>
2020-02-26 21:39:49 -0800

    added title text to tools

--- src/TimelineTools.js
+++ src/TimelineTools.js
@@ -10,24 +10,28 @@ export default class TimelineTools {
     let t = document.getElementById('timeline')
     return m('#TTools.tools', {}, [
       m('i', {
+        title: 'time selector',
           onclick: (vnode) => {
             t.style.cursor = 'text'
             state.tool('time')
           }
       }, '|'),
       m('i', {
+        title: 'trim',
         onclick: (vnode) => {
           t.style.cursor = 'ew-resize'
           state.tool('trim')
         }
       }, '[ ]'),
       m('i.material-icons', {
+        title: 'cut',
         onclick: (vnode) => {
           t.style.cursor = 'url(content-cut.svg), auto'
           state.tool('cut')
         }
       }, 'content_cut'),
       m('i.material-icons', {
+        title: 'move',
         onclick: (vnode) => {
           t.style.cursor = 'url(swap-horizontal.svg), -webkit-grab'
           state.tool('move')
