6e04ba356221f4bbc5fc90c64d6fa51027d38433
Harpo <harpo@harpo.me>
2020-03-12 20:21:38 -0700

    added /token request!!!

--- website/src/Login.js
+++ website/src/Login.js
@@ -1,6 +1,7 @@
 import m from 'mithril'
 import { Menu } from './Menu'
 import { User } from './User'
+import jwt_decode from 'jwt-decode'
 
 const Login = () => {
   return {
@@ -25,9 +26,52 @@ const Login = () => {
           m('br'),
           m('input#submit', {
             type: 'submit',
+            onclick: (e) => {
+              e.preventDefault()
+              let form = new FormData(document.getElementById('login'))
+              console.log('sending form', e, form)
+              let request = new XMLHttpRequest();
+              request.open('POST', '/token')
+              request.onreadystatechange = () => {
+                if (request.readyState == 4 && request.status == 200) {
+                  let token = JSON.parse(request.response)
+                  let decoded = jwt_decode(token['access_token'])
+                  console.log('authenticated!', decoded)
+                  m.route.set('/')
+                } else if (request.readyState == 4 && request.status != 200) {
+                  console.log('error logging in!', request)
+                }
               }
+              request.send(form)
+              // .then((e) => {
+              // })
+              // m.request({
+              //   url: '/token',
+              //   method: 'POST',
+              //   serialize: m.route.buildQueryString,
+              //   headers: {
+              //     'X-Requested-With': 'XMLHttpRequest',
+              //     'Content-Type':'application/x-www-form-urlencoded',
+              //   },
+              //   // config: function(xhr) {
+              //   //   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+              //   //   xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded')
+              //   // },
+              //   // data: {
+              //   //   username: document.getElementById('username').value,
+              //   //   password: document.getElementById('password').value,
+              //   // },
+              //   data: form,
+              // }).then((res) => {
+              //   console.log('authenticated!', res)
+              //   if (res['token']) {
+              //     User.token = res['type'] + ' ' + res['token']
+              //   }
+              // })
+            }},
           ),
           ]
+        )
       ]
     }
   }
