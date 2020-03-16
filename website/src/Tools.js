8659039d2c94b1261b9cd9d7886be96fe028e1bf
Harpo <harpo@harpo.me>
2020-03-15 19:28:05 -0700

    added save button!

--- website/src/Tools.js
+++ website/src/Tools.js
@@ -25,58 +25,65 @@ export default class Tools {
         title: 'back 5s',
         onclick: (vnode) => {
           Monitor.seekEdl(Edl.time - 5 || 0)}
-      }, 'fast_rewind'),
-      m('i.material-icons', {
-        title: 'pause',
-        onclick: Monitor.play,
-      }, Video.paused ? 'play_arrow': 'pause' ),
-      m('i.material-icons', {
-        title: 'forward 5s',
-        onclick: (vnode) => {
-          Monitor.seekEdl(Math.min(Edl.time + 5, Edl.duration()))}
-      }, 'fast_forward'),
-      m('i.material-icons', {
-        title: 'end',
-        onclick: (vnode) => {Monitor.seekEdl(Edl.duration())}
-      }, 'last_page'),
-      m('i.material-icons', {
-        title: 'slower',
-        onclick: (vnode) => {Monitor.slower()}
-      }, 'slow_motion_video'),
-      m('i', {
-        title: 'reset speed',
-        onclick: (vnode) => {Monitor.resetSpeed()}
-      }, Video.speed + 'x'),
-      m('i.material-icons', {
-        title: 'faster',
-        onclick: (vnode) => {Monitor.faster()}
-      }, 'speed'),
-      m('i.material-icons', {
-        title: 'render',
-        onclick: (vnode) => {
-          console.log('export')
-          message(`Added ${m.route.param("edl")} to render queue`)
-          m.request({
-            url: '/edl',
-            method: 'post',
-            params: {
-              filename: m.route.param('edl')
-            },
-            body: {'edl': Edl.edl}
-          }).then(e => {
-            message(`saved! ${e['filename']}`)
-            m.request({
-              url: '/render',
-              params: { edl: m.route.param('edl') },
-            }).then(e => {
-              console.log('got result', e)
-              m.route.set('/renders')
-              // success(`Successfully rendered ${m.route.param('edl')}`)
-              // downloadFile('/download?filename=' + m.route.param('edl') + '.mp4')
-            })
-          })
-        }
-      }, 'file_download')
-    ])
+        }, 'fast_rewind'),
+        m('i.material-icons', {
+          title: 'pause',
+          onclick: Monitor.play,
+        }, Video.paused ? 'play_arrow': 'pause' ),
+        m('i.material-icons', {
+          title: 'forward 5s',
+          onclick: (vnode) => {
+            Monitor.seekEdl(Math.min(Edl.time + 5, Edl.duration()))}
+          }, 'fast_forward'),
+          m('i.material-icons', {
+            title: 'end',
+            onclick: (vnode) => {Monitor.seekEdl(Edl.duration())}
+          }, 'last_page'),
+          m('i.material-icons', {
+            title: 'slower',
+            onclick: (vnode) => {Monitor.slower()}
+          }, 'slow_motion_video'),
+          m('i', {
+            title: 'reset speed',
+            onclick: (vnode) => {Monitor.resetSpeed()}
+          }, Video.speed + 'x'),
+          m('i.material-icons', {
+            title: 'faster',
+            onclick: (vnode) => {Monitor.faster()}
+          }, 'speed'),
+          m('i.material-icons', {
+            title: 'save',
+            onclick: vnode => {
+              m.request({
+                url: '/edl',
+                method: 'post',
+                params: {
+                  filename: m.route.param('edl')
+                },
+                body: {'edl': Edl.edl}
+              }).then(e => {
+                message(`saved! ${e['filename']}`, 3)
+              })
+            }
+          }, 'save'),
+          m('i.material-icons', {
+            title: 'render',
+            onclick: (vnode) => {
+              // console.log('export')
+              message(`Added ${m.route.param("edl")} to render queue`, 5)
+              m.request({
+                url: '/render',
+                params: { edl: m.route.param('edl') },
+              }).then(e => {
+                console.log('got result', e)
+                m.route.set('/renders')
+                // success(`Successfully rendered ${m.route.param('edl')}`)
+                // downloadFile('/download?filename=' + m.route.param('edl') + '.mp4')
+              }
+            )
+          }
+        }, 'file_download'),
+      ]
+    )
   }
 }
