b49881b6d1019476454a60616b91253155b6aea3
Harpo <harpo@harpo.me>
2020-03-31 12:12:18 -0700

    moved login to function in tests!

--- website/cypress/integration/login.js
+++ website/cypress/integration/login.js
@@ -1,7 +1,8 @@
+const username = 'johndoe'
+const password = 'secret'
+
 describe('the login page', () => {
   it('sets auth header when logging in for server authentication', () => {
-    const username = 'johndoe'
-    const password = 'secret'
     cy.visit('http://localhost:8000/')
     cy.contains('menu').click()
     cy.contains('login').click()
@@ -9,10 +10,15 @@ describe('the login page', () => {
     cy.contains('menu').click()
     cy.contains('projects').click()
     cy.url().should('include', '/#!/login')
-    cy.get('input[name=username]').type(username)
-    cy.get('input[name=password]').type(`${password}{enter}`)
-    cy.url().should('eq', 'http://localhost:8000/#!/')
+    autoLogin()
     cy.get(':nth-child(6) > #projects-link').click()
     cy.contains('demo.csv')
   })
 })
+
+export function autoLogin() {
+  cy.visit('http://localhost:8000/#!/login')
+  cy.get('input[name=username]').type(username)
+  cy.get('input[name=password]').type(`${password}{enter}`)
+  cy.url().should('eq', 'http://localhost:8000/#!/')
+}
