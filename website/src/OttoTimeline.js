74020221364e0dc73bbda32cb03fa3c939dde1cc
Harpo <harpo@harpo.me>
2020-06-08 21:10:53 -0700

    more robust array_move()

--- website/src/OttoTimeline.js
+++ website/src/OttoTimeline.js
@@ -8,19 +8,9 @@ import { Video, Edl } from './Video'
 import Monitor from './Monitor'
 
 function array_move(arr, old_index, new_index) {
-    while (old_index < 0) {
-        old_index += arr.length;
-    }
-    while (new_index < 0) {
-        new_index += arr.length;
-    }
-    if (new_index >= arr.length) {
-        var k = new_index - arr.length + 1;
-        while (k--) {
-            arr.push(undefined);
-        }
-    }
-    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    let element = arr[old_index];
+    arr.splice(old_index, 1);
+    arr.splice(new_index, 0, element);
     return arr
 };
 
