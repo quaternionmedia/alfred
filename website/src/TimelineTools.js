65292e28aec2dc1e7ca6106119f8aa42af4ca1f4
Harpo <harpo@harpo.me>
2020-06-16 21:39:06 -0700

    added highlight for TimelineTools

--- website/src/TimelineTools.js
+++ website/src/TimelineTools.js
@@ -14,6 +14,9 @@ export default class TimelineTools {
           onclick: (vnode) => {
             t.style.cursor = 'text'
             state.tool('time')
+          },
+          style: {
+            'background-color': state.tool() == 'time' ? '#393939' : 'transparent'
           }
       }, '|'),
       m('i', {
@@ -21,6 +24,9 @@ export default class TimelineTools {
         onclick: (vnode) => {
           t.style.cursor = 'ew-resize'
           state.tool('trim')
+        },
+        style: {
+          'background-color': state.tool() == 'trim' ? '#393939' : 'transparent'
         }
       }, '[ ]'),
       m('i.material-icons', {
@@ -28,6 +34,9 @@ export default class TimelineTools {
         onclick: (vnode) => {
           t.style.cursor = 'url(content-cut.svg), auto'
           state.tool('cut')
+        },
+        style: {
+          'background-color': state.tool() == 'cut' ? '#393939' : 'transparent'
         }
       }, 'content_cut'),
       m('i.material-icons', {
@@ -35,6 +44,9 @@ export default class TimelineTools {
         onclick: (vnode) => {
           t.style.cursor = 'url(swap-horizontal.svg), -webkit-grab'
           state.tool('move')
+        },
+        style: {
+          'background-color': state.tool() == 'move' ? '#393939' : 'transparent'
         }
       }, 'swap_horizontal'),
       m('i.material-icons', {
@@ -42,6 +54,9 @@ export default class TimelineTools {
         onclick: (vnode) => {
           t.style.cursor = 'ew-resize'
           state.tool('slip')
+        },
+        style: {
+          'background-color': state.tool() == 'slip' ? '#393939' : 'transparent'
         }
       }, 'swap_calls'),
       m('i.material-icons', {
@@ -49,6 +64,9 @@ export default class TimelineTools {
         onclick: (vnode) => {
           t.style.cursor = 'ew-resize'
           state.tool('slide')
+        },
+        style: {
+          'background-color': state.tool() == 'slide' ? '#393939' : 'transparent'
         }
       }, 'sync_alt'),
       m('i.material-icons', {
@@ -56,6 +74,9 @@ export default class TimelineTools {
         onclick: (vnode) => {
           t.style.cursor = 'ew-resize'
           state.tool('dual roller')
+        },
+        style: {
+          'background-color': state.tool() == 'dual roller' ? '#393939' : 'transparent'
         }
       }, 'compare_arrows'),
     ])
