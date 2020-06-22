396d0f34bd31b7746af5ac0cee0e86f734d138d2
Harpo <harpo@harpo.me>
2020-06-22 14:19:44 -0700

    added TextArea, Color to types, better formatting

--- website/src/Form.js
+++ website/src/Form.js
@@ -35,6 +35,37 @@ export function Form() {
       }
     }
   }
+  function TextArea() {
+    return {
+      view: (vnode) => {
+        return m('', {}, [
+          m('label.formlabel', {
+            for: vnode.attrs.name
+          }, vnode.children),
+          m('textarea', {
+            ...vnode.attrs,
+            value: selected ? selected[vnode.attrs.name] : null
+          })
+        ])
+      }
+    }
+  }
+  function Color() {
+    return {
+      view: (vnode) => {
+        return m('', {}, [
+          m('label.formlabel', {
+            for: vnode.attrs.name
+          }, vnode.children),
+          m('input', {
+            type: 'color',
+            ...vnode.attrs,
+            value: selected ? selected[vnode.attrs.name] : null
+          })
+        ])
+      }
+    }
+  }
 
   return {
     oninit: (vnode) => {
@@ -63,26 +94,30 @@ export function Form() {
         m('br'),
         m('label', {for: 'project'}, 'Project Name'),
         m('input', {name: 'project', value: selected ? selected.name: null}),
-        m('input', {type: 'submit', name: 'save', value: 'save'},),
+        m('', {style: {'text-align': 'right'}}, [
+          m('input', {type: 'submit', name: 'save', value: 'save'},),
+        ]),
         m('hr'),
         m(Text, {name: 'NAME'}, 'Business Name'),
         m(Text, {name: 'LOGO'}, 'Logo'),
-        m(Text, {name: 'ADDRESS'}, 'Address'),
+        m(TextArea, {name: 'ADDRESS'}, 'Address'),
         m(Text, {name: 'PHONE'}, 'Phone'),
         m(Text, {name: 'HOURS'}, 'Hours'),
         m(Text, {name: 'WEBSITE'}, 'Website'),
-        m(Text, {name: 'INITIAL'}, 'Initial text'),
-        m(Text, {name: 'BULLETS'}, 'Bullets'),
-        m(Text, {name: 'OPTIONAL'}, 'Optional'),
-        m(Text, {name: 'AUDIO'}, 'Audio'),
-        m(Text, {name: 'MEDIA'}, 'Media'),
-        m(Text, {name: 'CALL'}, 'Call'),
-        m(Text, {name: 'CLOSING'}, 'Closing'),
-        m(Text, {name: 'FONTCOLOR'}, 'Font Clolor'),
-        m(Text, {name: 'THEMECOLOR'}, 'Theme Color'),
+        m(TextArea, {name: 'INITIAL'}, 'Initial text'),
+        m(TextArea, {name: 'BULLETS'}, 'Bullets'),
+        m(TextArea, {name: 'OPTIONAL'}, 'Optional'),
+        m(TextArea, {name: 'AUDIO'}, 'Audio'),
+        m(TextArea, {name: 'MEDIA'}, 'Media'),
+        m(TextArea, {name: 'CALL'}, 'Call'),
+        // m(TextArea, {name: 'CLOSING'}, 'Closing'),
+        m(Color, {name: 'FONTCOLOR'}, 'Font Clolor'),
+        m(Color, {name: 'THEMECOLOR'}, 'Theme Color'),
         m(Text, {name: 'FONT'}, 'Font'),
         m(Text, {name: 'DURATION'}, 'Duration'),
-        m('input', {type: 'submit', name: 'render', value: 'render'}, )
+        m('', {style: {'text-align': 'right'}}, [
+          m('input', {type: 'submit', name: 'render', value: 'render'}, )
+        ]),
       ])
     }
   }
