e7aba24e543e843cb5199aa6187df2968453d828
Harpo <harpo@harpo.me>
2020-07-06 22:17:36 -0700

    integrated new field types into form

--- website/src/Form.js
+++ website/src/Form.js
@@ -1,7 +1,7 @@
 import m from 'mithril'
 import { success, error } from 'alertifyjs'
 import { Menu } from './Menu'
-
+import { Field, Input, InputArea, VideoForm } from './Input'
 
 export function Form() {
 
@@ -18,9 +18,12 @@ export function Form() {
             if (options && i) {
               m.request(`/project/${options[i - 1]}`).then(res => {
                 selected = res['form']
+                for (const property in selected) {
+                  console.log('setting VideoForm', property, selected[property])
+                  VideoForm[property](selected[property])
+                }
                 selected.project = options[i - 1]
                 console.log('selected', selected)
-                // selected['name'] = options[i - 1]
               })
             }
           }
@@ -116,23 +119,24 @@ export function Form() {
         ]),
         m('hr'),
 
-        m(Text, {name: 'name'}, 'Business Name'),
-        m(Text, {name: 'logo'}, 'Logo'),
-        m(TextArea, {name: 'address'}, 'Address'),
-        m(Text, {name: 'phone'}, 'Phone'),
-        m(Text, {name: 'hours'}, 'Hours'),
-        m(Text, {name: 'website'}, 'Website'),
-        m(TextArea, {name: 'initial'}, 'Initial text'),
-        m(TextArea, {name: 'bullets'}, 'Bullets'),
-        m(TextArea, {name: 'optional'}, 'Optional'),
-        m(TextArea, {name: 'media'}, 'Media'),
-        m(TextArea, {name: 'audio'}, 'Audio'),
-        m(TextArea, {name: 'call'}, 'Call'),
+        m(Input, {name: 'name'}, 'Business Name'),
+        m('p', VideoForm.name()),
+        m(Input, {name: 'logo'}, 'Logo'),
+        m(InputArea, {name: 'address'}, 'Address'),
+        m(Input, {name: 'phone'}, 'Phone'),
+        m(Input, {name: 'hours'}, 'Hours'),
+        m(Input, {name: 'website'}, 'Website'),
+        m(InputArea, {name: 'initial'}, 'Initial text'),
+        m(InputArea, {name: 'bullets'}, 'Bullets'),
+        m(InputArea, {name: 'optional'}, 'Optional'),
+        m(InputArea, {name: 'media'}, 'Media'),
+        m(InputArea, {name: 'audio'}, 'Audio'),
+        m(InputArea, {name: 'call'}, 'Call'),
         // m(TextArea, {name: 'closing'}, 'Closing'),
         m(Color, {name: 'themecolor', value:'#FF0'}, 'Theme Color'),
         m(Color, {name: 'fontcolor', value: '#FFF'}, 'Font Clolor'),
-        m(Text, {name: 'font'}, 'Font'),
-        m(Text, {name: 'duration'}, 'Duration'),
+        m(Input, {name: 'font'}, 'Font'),
+        m(Input, {name: 'duration'}, 'Duration'),
         m('hr'),
         m('', {style: {'text-align': 'right'}}, [
           m('input', {type: 'submit', name: 'render', value: 'Preview',
