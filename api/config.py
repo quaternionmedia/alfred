3c0518e598a85cb535f82f3665e97567680e5718
Harpo <harpo@harpo.me>
2020-07-21 14:23:38 -0700

    bucket name in env with default fallback

--- api/config.py
+++ api/config.py
@@ -7,4 +7,4 @@ DB_URL = environ.get('DB_URL')
 CELERY_BROKER = environ.get('CELERY_BROKER')
 CELERY_BACKEND = environ.get('CELERY_BACKEND')
 
-BUCKET_NAME = 'tower-bucket'
+BUCKET_NAME = environ.get('ALFRED_BUCKET_NAME', 'tower-renders')
