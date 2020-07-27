438e429d2c2edb503e496b4de79354f489693fec
Harpo <harpo@harpo.me>
2020-07-27 13:17:14 -0700

    created api bucket handler

---
+++ api/bucket.py
@@ -0,0 +1,22 @@
+from datetime import timedelta
+from google.cloud import storage
+from config import BUCKET_NAME
+
+def get_bucket(bucket=BUCKET_NAME):
+    client = storage.Client()
+    return client.bucket(bucket)
+
+def generate_signed_url(filename):
+    """Generates a v4 signed URL for downloading a blob.
+
+    Note that this method requires a service account key file.
+    """
+    blob = get_bucket().blob(filename)
+
+    return blob.generate_signed_url(
+        version="v4",
+        # This URL is valid for 15 minutes
+        expiration=timedelta(minutes=15),
+        # Allow GET requests using this URL.
+        method="GET",
+    )
