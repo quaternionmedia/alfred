d2732a15bb2bb225abf9dcbb4bb64e2d806b89a6
Harpo <harpo@harpo.me>
2020-08-05 11:14:10 -0700

    added inverseLog to sync slider position

--- website/src/Scale.js
+++ website/src/Scale.js
@@ -4,24 +4,29 @@ import { Video } from './Video'
 var Timeline = require('./Timeline').Timeline
 var state = require("./Globals").state
 
+// position will be between 0 and 1
+var minp = 0;
+var maxp = 1;
+
+// The result should be between .1 an 1000
+var minv = Math.log(.1);
+var maxv = Math.log(1000);
+
+// calculate adjustment factor
+var scale = (maxv-minv) / (maxp-minp);
+
 function logslider(position) {
-  // position will be between 0 and 1
-  var minp = 0;
-  var maxp = 1;
 
-  // The result should be between .1 an 1000
-  var minv = Math.log(.1);
-  var maxv = Math.log(1000);
+  return Math.exp( minv + scale * ( position - minp ) );
+}
 
-  // calculate adjustment factor
-  var scale = (maxv-minv) / (maxp-minp);
-
-  return Math.exp(minv + scale*(position-minp));
+function inverseLog(value) {
+  return ( ( Math.log(value) - minv ) / scale ) + minp
 }
 
 module.exports = {
   view: (vnode) => {
-    return m('.slider#scale', {min: 1, max: 100, style: {paddingLeft: '66%', position: 'relative'}})
+    return m('.slider#scale', {min: 1, max: 100, style: {paddingLeft: `${inverseLog(state.scale())*100}%`, 'background-color': '#393939'}})
   },
   oncreate: (vnode) => {
     const slider = interact(vnode.dom)
@@ -38,9 +43,9 @@ module.exports = {
         // const sliderWidth = interact.getElementRect(event.target.parentNode).width
         let sliderWidth = event.target.offsetWidth
         let p = event.pageX / sliderWidth
-        event.target.style.paddingLeft = p*100 + '%'
+        // event.target.style.paddingLeft = p*100 + '%'
         const value = logslider(p).toFixed(4)
-        // console.log('scale:', value, 'padding:', p)
+        console.log('scale:', value, 'padding:', p)
         // event.target.setAttribute('data-value', value)
         state.scale(value)
         m.redraw()
