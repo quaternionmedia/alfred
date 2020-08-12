0277b70deaa8a69807000d04746fe18d089a4e5e
Harpo <harpo@harpo.me>
2020-08-12 16:59:37 -0700

    prevent default on document 'Space' to stop scrolling on play

--- website/src/Preview.js
+++ website/src/Preview.js
@@ -64,6 +64,7 @@ export var Preview = ( () => {
             Edl.jump(Math.min(Edl.time + 5, Edl.duration()))
             break
           case 'Space':
+            e.preventDefault()
             Edl.play()
             break
         }
