b2905e9b531ae6438c0b0a5aeba6a26915acfc88
Harpo <harpo@harpo.me>
2021-02-19 19:00:10 -0800

    buildEdl in js after form submit

--- website/src/Magnussens.js
+++ website/src/Magnussens.js
@@ -42,20 +42,48 @@ export function Magnussens() {
           onclick: e => {
             e.preventDefault()
             let form = new FormData(document.getElementById('MagnussensForm'))
-            console.log('saving form', e, form)
-            message('rendering!')
-            m.request('/save', {
+            // form.forEach(f => {console.log('field', f.name, f)})
+            message('assembling render')
+            let edl = buildEdl(form.get('Car name'), form.get('Offer info'), form.get('Legal text'))
+            console.log('saving form', e, edl, form)
+            
+            
+            m.request('/render', {
               method: 'post',
               params: {
                 project: 'Magnussens'
               },
-              body: form
+              body: edl
             }).then(e => {
-              // message('rendering!')
+              success('Rendering!')
             })
           },
         },),
       ])]
     }
+  }
+}
+
+function buildEdl(car, offer, legal) {
+  return {
+    time: 30,
+    edl: [
+      {
+        type: 'video',
+        name: 'https://storage.googleapis.com/tower-bucket/alfred/car/Magnussens%20(check%20out%20offer).mp4',
+        duration: 30,
+        start: 0,
+      },
+      {
+        type: 'template',
+        name: 'title',
+        duration: 6,
+        start: 17.5,
+        data: {
+          text: car,
+          clipsize: [1280,720],
+        },
+      }
+    ]
   }
 }
