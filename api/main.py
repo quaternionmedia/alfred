132122a0488775b67fc0ffa81e51ce1ef780818b
Harpo <harpo@harpo.me>
2021-03-03 10:37:07 -0800

    split renderAPI into own file

--- api/main.py
+++ api/main.py
@@ -8,24 +8,23 @@ from os.path import join, isfile
 from os import listdir
 from urllib.request import urlopen
 from subprocess import run as bash
-from auth import auth, get_current_active_user, User, Edl
-from bson.json_util import dumps, ObjectId
+from auth import auth, get_current_active_user, User
+from bson.json_util import dumps
 
 from db import db
 from users import users
 from logger import DbLogger
 from seed import seed, formToEdl
-from tasks import renderRemote
 
+from render import renderAPI
 from otto.main import app as ottoApi
+
 from otto.render import renderEdl, renderForm
 from otto.getdata import timestr, download
 from otto.models import Edl, VideoForm
 
 from moviepy.editor import ImageClip, VideoFileClip
 from math import floor
-from config import BUCKET_NAME
-from bucket import generate_signed_url
 from emailer import sendMail
 
 def seconds(t):
@@ -121,76 +120,6 @@ async def saveEdl(filename: str, edl: Edl):
 @app.get('/download')
 async def download_file(filename: str):
     return FileResponse(filename, filename=filename)
-
-
-@app.post('/render')
-async def queueRender(prog: BackgroundTasks, project: str, width: int = 1920, height: int = 1080, edl: Edl = Body(...), user: User = Depends(get_current_active_user)):
-    ts = timestr()
-    filename = f'{project}_{width}x{height}_{edl.duration}s_{ts}.mp4'
-    render = {
-        'project': project,
-        'filename': filename,
-        'duration': edl.duration,
-        'resolution': (width, height),
-        'edl': edl.edl,
-        'progress': 0,
-        'started': ts,
-        'link': join('https://storage.googleapis.com/', BUCKET_NAME, filename),
-        }
-    # media = db.projects.find_one({'name': project}, ['form'])['form']['media']
-    id = db.renders.insert_one(render).inserted_id
-    print('rendering!', render)
-    # media = [ download(m) for m in proj['media'] ]
-    task = renderRemote.delay(edl=edl.edl, filename=filename, moviesize=(width, height))
-    def updateRenderProgress(progress):
-        r = progress.get('result')
-        # print('updating progress', r)
-        if r:
-            if r.get('index'):
-                p = max((100 * r['index'] / r['total']) - 1, 0)
-                db.renders.update_one({'filename': filename}, {'$set': {'progress': p}})
-            elif r.get('status') == 'uploaded':
-                db.renders.update_one({'filename': filename}, {'$set': {'progress': 100}})
-            else:
-                print('got other progress message', r)
-        else:
-            print('got other message', progress)
-    prog.add_task(task.get,
-                    on_message = updateRenderProgress,
-                    propagate=False)
-    return str(id)
-
-@app.get('/render')
-def getSignedRenderLink(name: str, user: User = Depends(get_current_active_user)):
-    return generate_signed_url(name)
-
-
-@app.get('/renders')
-def renders(user: User = Depends(get_current_active_user)):
-    return dumps(db.renders.find({}, ['filename', 'progress', 'link', 'project', 'resolution', 'duration', 'started']).sort([('_id', -1)]))
-
-
-@app.get('/renders/{render}')
-def rendersInfo(user: User = Depends(get_current_active_user)):
-    info = { 'edl': render, 'progress': 0, 'link': '', 'paused': False }
-    return info
-
-
-@app.put('/renders/{render}/pause')
-def pauseRender(user: User = Depends(get_current_active_user)):
-    # pause selected render
-    return
-
-
-@app.put('/renders/{render}/cancel')
-def cancelRender(render: str, user: User = Depends(get_current_active_user)):
-    # cancel selected render
-    res = db.renders.delete_one({'filename': render})
-    if res.deleted_count:
-        print('deleted render', render, res, res.deleted_count)
-        return res.deleted_count
-    else:
-        return HTTPException(status_code=406, detail='no such entry in database')
 
 
 @app.get('/edls')
@@ -303,9 +232,9 @@ async def reportIssue(name: str, issue: str = Body(...)):
         raise HTTPException(status_code=500, detail='error sending email')
     
 
+app.include_router(renderAPI)
 app.include_router(auth)
 app.include_router(users)
-
 # if request does not match the above api, try to return a StaticFiles match
 app.mount("/", StaticFiles(directory='dist', html=True), name="static")
 
---
+++ api/render.py
@@ -0,0 +1,80 @@
+from fastapi import APIRouter, Depends, Body, BackgroundTasks
+from auth import get_current_active_user, User
+from otto.models import Edl
+from otto.getdata import timestr
+from tasks import renderRemote
+from os.path import join
+from db import db
+from config import BUCKET_NAME
+from bson.json_util import dumps
+from bucket import generate_signed_url
+
+renderAPI = APIRouter()
+
+@renderAPI.post('/render')
+async def queueRender(prog: BackgroundTasks, project: str, width: int = 1920, height: int = 1080, edl: Edl = Body(...), user: User = Depends(get_current_active_user)):
+    ts = timestr()
+    filename = f'{project}_{width}x{height}_{edl.duration}s_{ts}.mp4'
+    render = {
+        'project': project,
+        'filename': filename,
+        'duration': edl.duration,
+        'resolution': (width, height),
+        'edl': edl.edl,
+        'progress': 0,
+        'started': ts,
+        'link': join('https://storage.googleapis.com/', BUCKET_NAME, filename),
+        }
+    # media = db.projects.find_one({'name': project}, ['form'])['form']['media']
+    id = db.renders.insert_one(render).inserted_id
+    print('rendering!', render)
+    task = renderRemote.delay(edl=edl.edl, filename=filename, moviesize=(width, height))
+    def updateRenderProgress(progress):
+        r = progress.get('result')
+        # print('updating progress', r)
+        if r:
+            if r.get('index'):
+                p = max((100 * r['index'] / r['total']) - 1, 0)
+                db.renders.update_one({'filename': filename}, {'$set': {'progress': p}})
+            elif r.get('status') == 'uploaded':
+                db.renders.update_one({'filename': filename}, {'$set': {'progress': 100}})
+            else:
+                print('got other progress message', r)
+        else:
+            print('got other message', progress)
+    prog.add_task(task.get,
+                    on_message = updateRenderProgress,
+                    propagate=False)
+    return str(id)
+
+@renderAPI.get('/render')
+def getSignedRenderLink(name: str, user: User = Depends(get_current_active_user)):
+    return generate_signed_url(name)
+
+
+@renderAPI.get('/renders')
+def renders(user: User = Depends(get_current_active_user)):
+    return dumps(db.renders.find({}, ['filename', 'progress', 'link', 'project', 'resolution', 'duration', 'started']).sort([('_id', -1)]))
+
+
+@renderAPI.get('/renders/{render}')
+def rendersInfo(user: User = Depends(get_current_active_user)):
+    info = { 'edl': render, 'progress': 0, 'link': '', 'paused': False }
+    return info
+
+
+@renderAPI.put('/renders/{render}/pause')
+def pauseRender(user: User = Depends(get_current_active_user)):
+    # pause selected render
+    return
+
+
+@renderAPI.put('/renders/{render}/cancel')
+def cancelRender(render: str, user: User = Depends(get_current_active_user)):
+    # cancel selected render
+    res = db.renders.delete_one({'filename': render})
+    if res.deleted_count:
+        print('deleted render', render, res, res.deleted_count)
+        return res.deleted_count
+    else:
+        return HTTPException(status_code=406, detail='no such entry in database')
