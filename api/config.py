185d2dca442a1af1a97c72546fd0db4fdd72785a
Harpo <harpo@harpo.me>
2021-03-18 10:47:41 -0700

    fixed error if no env for EMAIL_SENDTO

--- api/config.py
+++ api/config.py
@@ -13,7 +13,9 @@ EMAIL_USERNAME = environ.get('EMAIL_USERNAME')
 EMAIL_PASSWORD = environ.get('EMAIL_PASSWORD')
 EMAIL_SERVER = environ.get('EMAIL_SERVER')
 EMAIL_PORT = environ.get('EMAIL_PORT')
-EMAIL_SENDTO = environ.get('EMAIL_SENDTO').split(',')
+EMAIL_SENDTO = environ.get('EMAIL_SENDTO')
+if EMAIL_SENDTO:
+    EMAIL_SENDTO  = EMAIL_SENDTO.split(',')
 
 INVOICE_EMAIL_BODY = '''Please find attached an invoice for this week's renders. If you have any questions, feel free to contact us.
 
