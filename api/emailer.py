c39b49cf90f3be8949a6f67fab616e94de18bcd7
Harpo <harpo@harpo.me>
2021-03-17 17:44:36 -0700

    added attachment handler for sendMail

--- api/emailer.py
+++ api/emailer.py
@@ -2,16 +2,28 @@ from fastapi import APIRouter, Body
 from smtplib import SMTP
 from email.message import EmailMessage
 from config import EMAIL_PORT, EMAIL_SERVER, EMAIL_USERNAME, EMAIL_PASSWORD, EMAIL_SENDTO, INVOICE_EMAIL_BODY
+from mimetypes import guess_type
 
-def sendMail(recepients, subject, message, attachments=None):
+def sendMail(recepients, subject, message, attachments=[]):
     try:
         msg = EmailMessage()
         msg.set_content(message)
         msg['From'] = EMAIL_USERNAME
         msg['To'] = recepients
         msg['Subject'] = subject
-        s=SMTP(config.EMAIL_SERVER, config.EMAIL_PORT)
-        s.set_debuglevel(1)
+        if len(attachments):
+            for path in attachments:
+                ctype, encoding = guess_type(path)
+                if ctype is None or encoding is not None:
+                    # No guess could be made, or the file is encoded (compressed), so
+                    # use a generic bag-of-bits type.
+                    ctype = 'application/octet-stream'
+                maintype, subtype = ctype.split('/', 1)
+                with open(path, 'rb') as f:
+                    msg.add_attachment(f.read(), maintype=maintype, subtype=subtype, filename=path)
+        
+        s=SMTP(EMAIL_SERVER, EMAIL_PORT)
+        # s.set_debuglevel(1)
         s.starttls()
         s.login(EMAIL_USERNAME, EMAIL_PASSWORD)
         s.send_message(msg)
