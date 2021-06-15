bcdeba3759eed1e47c439cb0ba1446ff4365d259
Harpo <harpo@harpo.me>
2021-06-15 14:00:31 -0700

    fully replicated magnussens logic in json-logic-engine !!!

--- website/src/logic.js
+++ website/src/logic.js
@@ -8,6 +8,7 @@ const engine = new LogicEngine()
 engine.addMethod('floor', Math.floor)
 engine.addMethod('sqrt', Math.sqrt)
 
+
 const rules = ({ merge: [
   { eachKey: 
     {
@@ -19,17 +20,42 @@ const rules = ({ merge: [
     }},
     { eachKey: {
       type: 'template',
-      name: 'textBox',
-      duration: {var: 'duration'},
+      name: 'makeColor',
+      duration: 7.2,
       start: 17.1,
+      data: {preserve: {
+        color: [255,255,255],
+        opacity: 1,
+      }}
+    }},
+    { eachKey: {
+      type: 'image',
+      name: 'https://storage.googleapis.com/tower-bucket/alfred/car/magnussens-screengrab%20logo-fixed-with-toyota.png',
+      position: ['center', 'top'],
+      resize: {'/': [{sqrt: {'*': [{var: 'width'}, {var: 'height'}]}}, 3600]},
+      start: 17.1,
+      duration: 7.2,
+    }},
+    { eachKey: {
+      type: 'template',
+      name: 'textBox',
+      duration: 7.2,
+      start: 17.1,
+      // relative: false,
       data: { eachKey: {
         text: {var: 'carname'},
-        color: '#000000',
+        color: '#EB0A1E',
+        // color: '#000000',
       //   // textsize: [Math.floor(.9*width), Math.floor(.3*height)],
+      textsize: [{floor: {'*': [.9, {var: 'width'}]}}, {floor: {'*': [.3, {var: 'height'}]}}],
         font: 'Toyota-Type-Bold',
       //   // fontsize: Math.pow(width*height, .5)/15,
-        position: ['center', .1],
-        relative: true,
+        fontsize: {'/': [{sqrt: {'*': [{var: 'width'}, {var: 'height'}]}}, 15]},
+        // fontsize: {'/': [{var: 'width'}, {var: 'height'}]},
+        // position: [.9, .3],
+        // relative: true,
+        // position: 'center',
+        position: ['center', {floor: {'*': [.1, {var: 'height'}]}}],
         opacity: 1,
         fxs: {preserve: [{
           name: 'bezier2',
@@ -45,13 +71,42 @@ const rules = ({ merge: [
       }},
     }},
     { eachKey: {
-      type: 'image',
-      name: 'https://storage.googleapis.com/tower-bucket/alfred/car/magnussens-screengrab%20logo-fixed-with-toyota.png',
-      position: ['center', 'top'],
-      // resize: Math.pow(width*height, .5)/3600,
+      type: 'template',
+      name: 'textBox',
+      duration: 7.2,
       start: 17.1,
-      duration: {var: 'duration'},
-    }},
+      // position: [.5, .8 ],
+      relative: true,
+      data: {eachKey: {
+        color: '#EB0A1E',
+        text: {var: 'offerinfo'},
+        textsize: [{floor: {'*': [.9, {var: 'width'}]}}, {floor: {'*': [.5, {var: 'height'}]}}],
+        // textsize: [1000, 1000],
+        font: 'Toyota-Type',
+        fontsize: {'/': [{sqrt: {'*': [{var: 'width'}, {var: 'height'}]}}, 32]},
+        opacity: 1,
+        position: 'center',
+        // align: data.offeralign == 'left' ? 'west' : data.offeralign,
+        align: {if: [{'==': [{var: 'offeralign'}, 'left']}, 'west', {var: 'offeralign'}]},
+      }},
+    },},
+    { eachKey: {
+      type: 'template',
+      name: 'textBox',
+      duration: 7.2,
+      start: 17.1,
+      data: { eachKey: {
+        color: '#333333',
+        text: {var: 'legaltext'},
+        method: 'caption',
+        textsize: [{floor: {'*': [.9, {var: 'width'}]}}, {floor: {'*': [.35, {var: 'height'}]}}],
+        font: 'Toyota-Type-Book',
+        fontsize: {'/': [{sqrt: {'*': [{var: 'width'}, {var: 'height'}]}}, 60]},
+        position: ['center', 'bottom'],
+        align: 'west',
+        opacity: 1,
+      }}
+    },}
   ]})
 
 const magnussens = engine.build(rules)
