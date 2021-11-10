0eda8c5d0cd8c5a11f05c412bea1062138bad937
Harpo <harpo@harpo.me>
2021-11-10 12:13:35 -0800

    beginning of migration to fastapi_users

--- api/users.py
+++ api/users.py
@@ -1,53 +1,44 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
-from fastapi.param_functions import Form
-from auth import User, UserInDB, get_current_active_user, pwd_context
-from db import db
+from typing import Optional
+from fastapi import Depends, Request
+from fastapi_users import BaseUserManager, FastAPIUsers
+from fastapi_users.authentication import JWTAuthentication
+from fastapi_users.db import MongoDBUserDatabase
+
+from db import get_user_db
+from models import User, UserCreate, UserDB, UserUpdate
+
+from config import SECRET_KEY
+
+jwt_authentication = JWTAuthentication(
+    secret=SECRET_KEY, lifetime_seconds=3600, tokenUrl="token"
+)
+class UserManager(BaseUserManager[UserCreate, UserDB]):
+  user_db_model = UserDB
+  reset_password_token_secret = SECRET_KEY
+  verification_token_secret = SECRET_KEY
+
+  async def on_after_register(self, user: UserDB, request: Optional[Request] = None):
+    print(f"User {user.id} has registered.")
 
 
-class RegisterForm(OAuth2PasswordRequestForm):
-    def __init__(
-        self,
-        username: str = Form(...),
-        password: str = Form(...),
-        email: str = Form(...)
-    ):
-        self.email = email
-        self.username = username
-        self.password = password
-
-users = APIRouter()
-
-@users.post('/register')
-async def registerUser(form_data: RegisterForm = Depends()):
-    """# Regsister User
-    Create a new user in the database"""
-    c = db.users.find({'username': form_data.username})
-    if c.count():
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User already exists",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    else:
-        db.users.insert_one(UserInDB(
-            username=form_data.username,
-            hashed_password=pwd_context.hash(form_data.password),
-            email=form_data.email,
-            verified=False,
-        ).dict())
-        # TODO: send email verification here
-
-@users.get("/users/me/", response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
-    """# Who Am I
-    Returns a `User` object with the current logged in user."""
-    return current_user
+  async def on_after_forgot_password(self, user: UserDB, token: str, request: Optional[Request] = None):
+    print(f"User {user.id} has forgot their password. Reset token: {token}")
 
 
-@users.get("/users/{username}/", response_model=User)
-async def read_user(username: str, current_user: User = Depends(get_current_active_user)):
-    """# Read user
-    Returns a `User` object for the requested username"""
-    # TODO: Manage scopes and permissions
-    return db.users.find_one({'username': username})
+  async def on_after_request_verify(self, user: UserDB, token: str, request: Optional[Request] = None):
+    print(f"Verification requested for user {user.id}. Verification token: {token}")
+
+
+async def get_user_manager(user_db: MongoDBUserDatabase = Depends(get_user_db)):
+  yield UserManager(user_db)
+
+fastapi_users = FastAPIUsers(
+    get_user_manager,
+    [jwt_authentication],
+    User,
+    UserCreate,
+    UserUpdate,
+    UserDB,
+)
+
+current_active_user = fastapi_users.current_user(active=True)
