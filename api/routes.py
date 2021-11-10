0a830c6ea742c2bd81508e8aa75eb31569d11e1f
Harpo <harpo@harpo.me>
2021-11-10 11:07:31 -0800

    depricated v1 routes for forms, bkg, edl, etc.

--- api/routes.py
+++ api/routes.py
@@ -22,13 +22,6 @@ from math import floor
 
 routes = APIRouter()
 
-@routes.post('/edl')
-async def saveEdl(filename: str, edl: Edl):
-    return dumps(db.edls.find_one_and_update({'filename': filename}, {'$set': {'edl': edl.clips}}, upsert=True, new=True))
-
-@routes.get('/download')
-async def download_file(filename: str):
-    return FileResponse(filename, filename=filename)
 
 @routes.get('/fonts')
 async def getFonts():
@@ -52,64 +45,9 @@ async def getProjects():
     """# Get projects
     Returns a list of all project names available"""
     return [p['name'] for p in await db.projects.find({}, { 'name': 1, '_id': 0 }).to_list(length=100)]
-
+    
 
 @routes.get('/project/{project}')
 async def getProject(project: str):
-    res = db.projects.find_one({'name': project})
-    del res['_id']
-    return res
-
-@routes.post('/save')
-async def saveForm(project: str, form: VideoForm = Depends(VideoForm.as_form)):
-    form.media = [ m.strip() for m in form.media.split(',') ]
-    form.audio = [ m.strip() for m in form.audio.split(',') ]
-    result = db.projects.update_one({'name': project}, {'$set': {'form': dict(form)}}, upsert=True)
-    print('saved form', project, form, result)
-    return result.modified_count
-
-@routes.post('/formToEdl')
-async def form_to_edl(form: VideoForm = Depends(VideoForm.as_form)):
-    form.media = [ m.strip() for m in form.media.split(',') ]
-    form.audio = [ m.strip() for m in form.audio.split(',') ]
-    edl = formToEdl(form)
-    print('edl from form', edl, dict(form))
-    db.projects.update_one({'name': form.project},
-        {'$set':
-            {
-                'form': dict(form),
-                'edl': edl['edl'],
-            }}, upsert=True)
-    return True
-
-@routes.get('/bkg/{project}')
-async def get_bkg(project: str, width: int, height: int, t: float):
-    clips = db.projects.find_one({'name': project}, ['form'])['form']['media']
-    clip = download(clips[floor(t / 5)])
-    print('making bkg', clip)
-    try:
-        if clip.endswith('mp4'):
-            clip = VideoFileClip(clip)
-        else:
-            clip = ImageClip(clip)
-        if width >= height:
-            clip = clip.resize(width=width)
-        else:
-            clip = clip.resize(height=height)
-        clip.save_frame('bkg.jpg', t=t % 5)
-        return FileResponse('bkg.jpg')
-    except Exception as e:
-        print('error making kburns frame', e)
-        raise HTTPException(status_code=500, detail='error making kburns frame')
-
-@routes.post('/upload')
-async def upload(file: UploadFile = File(...)):
-    print('saving files', file.filename)
-    await saveFile(file)
-    return {'filename': join('data', file.filename)}
-
-async def saveFile(file, location='data'):
-    data = await file.read()
-    with open(join(location, file.filename), 'wb') as f:
-        f.write(data)
-
+    return await db.projects.find_one({'name': project}, {'_id': 0})
+    
--- api/v1.py
+++ api/v1.py
@@ -42,4 +42,73 @@ async def form_to_video(renderer: BackgroundTasks, form: VideoForm = Depends(Vid
         })
     renderer.add_task(renderForm, form=dict(form), filename=join('videos', filename), logger=DbLogger(filename))
     return filename
+    
 
+@routes.get('/download')
+async def download_file(filename: str):
+    """# Download
+    Download file from server
+    
+    *Depricated*
+    """
+    return FileResponse(filename, filename=filename)
+
+
+
+@routes.post('/upload')
+async def upload(file: UploadFile = File(...)):
+    print('saving files', file.filename)
+    await saveFile(file)
+    return {'filename': join('data', file.filename)}
+
+async def saveFile(file, location='data'):
+    data = await file.read()
+    with open(join(location, file.filename), 'wb') as f:
+        f.write(data)
+
+
+@routes.post('/save')
+async def saveForm(project: str, form: VideoForm = Depends(VideoForm.as_form)):
+    form.media = [ m.strip() for m in form.media.split(',') ]
+    form.audio = [ m.strip() for m in form.audio.split(',') ]
+    result = db.projects.update_one({'name': project}, {'$set': {'form': dict(form)}}, upsert=True)
+    print('saved form', project, form, result)
+    return result.modified_count
+
+@routes.post('/formToEdl')
+async def form_to_edl(form: VideoForm = Depends(VideoForm.as_form)):
+    form.media = [ m.strip() for m in form.media.split(',') ]
+    form.audio = [ m.strip() for m in form.audio.split(',') ]
+    edl = formToEdl(form)
+    print('edl from form', edl, dict(form))
+    db.projects.update_one({'name': form.project},
+        {'$set':
+            {
+                'form': dict(form),
+                'edl': edl['edl'],
+            }}, upsert=True)
+    return True
+
+@routes.get('/bkg/{project}')
+async def get_bkg(project: str, width: int, height: int, t: float):
+    clips = db.projects.find_one({'name': project}, ['form'])['form']['media']
+    clip = download(clips[floor(t / 5)])
+    print('making bkg', clip)
+    try:
+        if clip.endswith('mp4'):
+            clip = VideoFileClip(clip)
+        else:
+            clip = ImageClip(clip)
+        if width >= height:
+            clip = clip.resize(width=width)
+        else:
+            clip = clip.resize(height=height)
+        clip.save_frame('bkg.jpg', t=t % 5)
+        return FileResponse('bkg.jpg')
+    except Exception as e:
+        print('error making kburns frame', e)
+        raise HTTPException(status_code=500, detail='error making kburns frame')
+
+@routes.post('/edl')
+async def saveEdl(filename: str, edl: Edl):
+    return dumps(db.edls.find_one_and_update({'filename': filename}, {'$set': {'edl': edl.clips}}, upsert=True, new=True))
