da0dc15501c953233e4644d4ffc83fb8311b6494
Harpo <harpo@harpo.me>
2021-11-14 11:36:43 -0800

    updated birthday field from date to datetime, due to pymongo error

--- api/models.py
+++ api/models.py
@@ -1,21 +1,23 @@
 from fastapi_users import models
-from typing import Optional
-from datetime import date
+from pydantic import BaseModel
 from odmantic import Model
+from typing import Optional, List
+from datetime import datetime
 
 class User(models.BaseUser):
     first_name: str
-    birthdate: Optional[date]
+    birthdate: Optional[datetime]
 
 
 class UserCreate(models.BaseUserCreate):
     first_name: str
-    birthdate: Optional[date]
-
+    birthdate: Optional[datetime]
+    pass
 
 class UserUpdate(models.BaseUserUpdate):
-    first_name: str
-    birthdate: Optional[date]
+    first_name: Optional[str]
+    birthdate: Optional[datetime]
+    pass
 
 
 class UserDB(User, models.BaseUserDB):
