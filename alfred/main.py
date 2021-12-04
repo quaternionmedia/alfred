7c4aef5940fe4ecc9b3ef649c8d43e7cc987b046
Harpo <harpo@harpo.me>
2021-12-03 23:40:50 -0800

    generalize ProjectAPI

--- alfred/main.py
+++ alfred/main.py
@@ -57,7 +57,9 @@ app.include_router(fastapi_users.get_users_router(), prefix='/users', tags=['use
 
 projects = ProjectAPI()
 app.include_router(projects,
-    dependencies=[Depends(current_active_user)])
+    dependencies=[Depends(current_active_user)],
+    tags=['project']
+    )
 
 app.include_router(fontAPI,
     dependencies=[Depends(current_active_user)], tags=['font'])
