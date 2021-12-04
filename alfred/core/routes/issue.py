128a673726d0ca039ed85f84c240197bcce6b7e8
Harpo <harpo@harpo.me>
2021-12-04 10:05:53 -0800

    standardized item_id in issueAPI

--- alfred/core/routes/issue.py
+++ alfred/core/routes/issue.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, Body
 from ..utils import sendMail
+from alfred.config import EMAIL_SENDTO
 
 issueAPI = APIRouter()
 
-@issueAPI.post('/{render}/report')
-async def reportIssue(render: str, issue: str = Body(...)):
+@issueAPI.post('/{item_id}/report')
+async def reportIssue(item_id: str, issue: str = Body(...)):
     """# Report issue
     Reports an issue with a specific render. Sends an email to the support team for follow up."""
-    if not sendMail(recepients=EMAIL_SENDTO, subject=render, message=issue):
-        print('error reporting issue with ', render, issue)
+    if not sendMail(recepients=EMAIL_SENDTO, subject=f'issue with render: {item_id}', message=issue):
+        print('error reporting issue with ', item_id, issue)
         raise HTTPException(status_code=500, detail='error sending email')
