c0ccb8a901028ab8ed10c503913aaf0e10fb6b12
Harpo <harpo@harpo.me>
2022-01-18 19:26:09 -0800

    added color and image components for forms

--- website/src/Components.js
+++ website/src/Components.js
@@ -38,10 +38,33 @@ export function Button() {
   }
 }
 
+export function Color() {
+  return {
+    view: vnode => {
+      return [
+        m('label.formlabel', { for: vnode.attrs.name }, vnode.attrs.text),
+        m('input[type=color]', vnode.attrs),
+      ]
+    }
+  }
+}
+
 export function Img() {
   return {
     view: vnode => {
       return m('img', vnode.attrs, vnode.children)
+    }
+  }
+}
+
+export function Image() {
+  return {
+    view: vnode => {
+      return [
+        m('label.formlabel', { for: vnode.attrs.name }, vnode.attrs.text),
+        m('input[type=text]', vnode.attrs),
+        m('img', {src: vnode.attrs.value}, vnode.children)
+      ]
     }
   }
 }
