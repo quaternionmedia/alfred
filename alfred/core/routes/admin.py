9ac7379d56886f74ee060062fcf72aa24d3fdaf0
Harpo <harpo@harpo.me>
2022-07-19 18:15:36 -0700

    Fix imports

--- alfred/core/routes/admin.py
+++ alfred/core/routes/admin.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
 from datetime import datetime, date, time
-from datetime.timezone import utc
+from datetime import timezone
 from ..utils import sendMail
 from .invoicer import generate_invoice
-from config import INVOICE_EMAIL_BODY, EMAIL_SENDTO
+from alfred.config import INVOICE_EMAIL_BODY, EMAIL_SENDTO
 
 adminAPI = APIRouter()
 
@@ -14,8 +14,8 @@ async def sendInvoice(client: str, startDate: date, endDate: date):
     """# Send Invoice
     Generates a PDF invoice and sends an email to the client for all renders executed in the specified period"""
     midnight = time(0)
-    startDate = datetime.combine(startDate, midnight, utc)
-    endDate = datetime.combine(endDate, midnight, utc)
+    startDate = datetime.combine(startDate, midnight, timezone.utc)
+    endDate = datetime.combine(endDate, midnight, timezone.utc)
     invoices = generate_invoice(client, startDate, endDate)
     body = INVOICE_EMAIL_BODY
     if not sendMail(
