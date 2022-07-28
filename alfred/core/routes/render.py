ce2c4062fd0ea4e93d0ee45b55ca25c091ee9371
Harpo <harpo@harpo.me>
2022-07-27 20:49:12 -0700

    Format render with black

--- alfred/core/routes/render.py
+++ alfred/core/routes/render.py
@@ -13,31 +13,36 @@ from alfred.config import DB_URL, DB_NAME
 from ..utils.db import get_client
 
 class RenderAPI(MotorCRUDRouter):
-    def __init__(self, 
-            schema = Render,
-            create_schema = Render,
-            update_schema = RenderUpdate,
-            db_name = DB_NAME,
-            *args, **kwargs):
+    def __init__(
+        self,
+        schema=Render,
+        create_schema=Render,
+        update_schema=RenderUpdate,
+        db_name=DB_NAME,
+        *args,
+        **kwargs,
+    ):
         self.schema = schema
-        self.create_schema = create_schema,
-        self.update_schema = update_schema,
+        self.create_schema = (create_schema,)
+        self.update_schema = (update_schema,)
         self.db_name = db_name
         self.client = get_client()
         # self.db = self.client[DB_NAME]
-        super().__init__(*args, **kwargs, 
-            schema = schema,
-            client = self.client,
-            create_schema = create_schema,
-            update_schema = update_schema,
-            db_url = DB_URL,
-            database = DB_NAME,
-    )
-    
+        super().__init__(
+            *args,
+            **kwargs,
+            schema=schema,
+            client=self.client,
+            create_schema=create_schema,
+            update_schema=update_schema,
+            db_url=DB_URL,
+            database=DB_NAME,
+        )
+
         @self.post('', *args, **kwargs)
         async def queueRender(
-                render: Render,
-                user: User = Depends(current_active_user)):
+            render: Render, user: User = Depends(current_active_user)
+        ):
             ts = timestr()
             filename = f'{render.project}{"_" + render.description if render.description else ""}_{render.width}x{render.height}{"_" + render.quality if render.quality else ""}_{render.edl.duration}s_{ts}.mp4'
             render.username = user.email
@@ -47,21 +52,22 @@ class RenderAPI(MotorCRUDRouter):
             result = await self.schema.insert_one(render)
             print('rendering!', render, result)
             task = renderRemote.delay(
-                edl=render.edl, 
+                edl=render.edl,
                 renderId=result.id,
                 filename=filename,
-                moviesize=(render.width, render.height), 
-                fps=render.fps, 
+                moviesize=(render.width, render.height),
+                fps=render.fps,
                 bitrate=render.bitrate,
-                ffmpeg_params=render.ffmpeg_params)
+                ffmpeg_params=render.ffmpeg_params,
+            )
             return str(result.id)
 
         @self.get('/{item_id}', *args, **kwargs)
-        async def getSignedRenderLink(item_id: str, user: User = Depends(current_active_user)):
+        async def getSignedRenderLink(
+            item_id: str, user: User = Depends(current_active_user)
+        ):
             return generate_signed_url(item_id)
-        
+
         @self.get('', *args, **kwargs)
-        async def get_all_renders(
-                skip: Optional[int] = 0, 
-                limit: Optional[int] = 100):
+        async def get_all_renders(skip: Optional[int] = 0, limit: Optional[int] = 100):
             return await self.schema.find_all().skip(skip).sort('-_id').to_list(limit)
