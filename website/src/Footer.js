c2d71804c56bac0cfe226053c6cc935946c0b1d0
Cameron West <Cameron.west@hotmail.com>
2022-08-12 17:53:09 -0500

    Updating Class names for style, progresses #76

--- website/src/Footer.js
+++ website/src/Footer.js
@@ -6,8 +6,8 @@ function Footer() {
         view: (vnode) => {
             return [  
                 m('footer', 
-                    m('.footer-text', "More information at " ),
-                    m('a.footer-link', {href: 'https://www.quaternion.media/alfred/'}, 'quaternion.media/alfred')
+                    m('.text', "More information at " ),
+                    m('a.link', {href: 'https://www.quaternion.media/alfred/'}, 'quaternion.media/alfred'), 
                 )
             ]
         }
